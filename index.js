0272197cc557d2e15f853784b057dff03ab577de
ToneDef00 <daniels@dual4t.com>
2021-02-25 18:06:23 -0800

    removed cors bs that didnt work

--- index.js
+++ index.js
@@ -38,11 +38,11 @@ mqtt.createMqttClient().then((mqttClient) => {
     //middleware
     app.use(express.json());
 
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "https://us-central1-agromation-grow-room-control.cloudfunctions.net"); // update to match the domain you will make the request from
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-      });
+    // app.use(function(req, res, next) {
+    //     res.header("Access-Control-Allow-Origin", "https://us-central1-agromation-grow-room-control.cloudfunctions.net"); // update to match the domain you will make the request from
+    //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //     next();
+    //   });
 
     //routes
 
